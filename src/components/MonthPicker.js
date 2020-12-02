10d42d81dec55fbedd368ef21dff619ddeb4d7ce
a23246542 <a23246542@gmail.com>
2020-12-03 00:52:27 +0800

    創建下拉選單，可切換顯示(不JQ)

--- src/components/MonthPicker.js
+++ src/components/MonthPicker.js
@@ -1,17 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { padLeft } from '../utility';
 
 const MonthPicker = ({year, month}) => {
+  const [isOpen, setOpen] = useState(false);
   return (
     <div className="dropdown">
       <p>請選擇</p>
-      <button className="btn btn-primary dropdown-toggle">
+      <button 
+        className="btn btn-primary dropdown-toggle"
+        onClick={()=>{ setOpen(!isOpen);}}
+      >
         {`${year}年 ${padLeft(month)}月`}
       </button>
-      <div className="dropdown-menu">
-        XXXxx
-      </div>
+      { isOpen&& 
+        <div className="dropdown-menu dropdown-menu-right" 
+            style={{display: 'block'}} //!!!BS4默認隱藏
+        >
+          XXX
+        </div>
+      }
     </div>
   )
 }
