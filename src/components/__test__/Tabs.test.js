47bad9e886af7bcaf11fc625bb449445bf1b8bdb
a23246542 <a23246542@gmail.com>
2020-12-10 18:08:19 +0800

    測試介面不要測試狀態

--- src/components/__test__/Tabs.test.js
+++ src/components/__test__/Tabs.test.js
@@ -13,12 +13,12 @@ let wrapper = {};
 describe('test Tabs and Tab component', ()=>{
   //進行每個最小用例之前，重新shallow
   beforeEach(()=>{
-    // wrapper = mount(
-    //   <Tabs {...props}>
-    //     <Tab>1</Tab>
-    //     <Tab>2</Tab>
-    //   </Tabs>
-    // )
+    wrapper = mount(
+      <Tabs {...props}>
+        <Tab>1</Tab>
+        <Tab>2</Tab>
+      </Tabs>
+    )
     // const { getByTestId } = render(
     //   <Tabs {...props}>
     //     <Tab>1</Tab>
@@ -39,7 +39,7 @@ describe('test Tabs and Tab component', ()=>{
   //   )
   //   expect(getByTestId('navTabs')).toBeInTheDocument();
   // })
-  //默認渲染兩個tab，第一個標籤要是active
+  //測試默認渲染render兩個tab(length)，第一個標籤要是active
   
   //交互點第二個tab會改變active狀態跟觸發對的函式
   test('click',()=>{
@@ -56,13 +56,18 @@ describe('test Tabs and Tab component', ()=>{
     expect(getByTestId('navTabs').lastChild.firstChild.classList.contains('active')).toBe(true)
     // expect(getByTestId('navTabs').firstChild.firstChild.toHaveClass('active')).toBe(true)
     //不測試state
-    
+
   })
-  // it('click the 2nd Tab should change the active stauts and trigger the right function', () => {
-  //   wrapper.find('.nav-link').last().simulate('click', { preventDefault: () => {}})
-  //   expect(wrapper.find('.nav-link').first().hasClass('active')).toEqual(false)
-  //   expect(wrapper.find('.nav-link').last().hasClass('active')).toEqual(true)
-  //   // expect(wrapper.instance().state.Index).toEqual(1)
-  //   // expect(props.onTabChange).toHaveBeenCalledWith(1)
-  // })
+  // 測試交互後的畫面變化
+  it('click the 2nd Tab should change the active stauts and trigger the right function', () => {
+    wrapper.find('.nav-link').last().simulate('click', { preventDefault: () => {}})
+    expect(wrapper.find('.nav-link').first().hasClass('active')).toEqual(false)
+    expect(wrapper.find('.nav-link').last().hasClass('active')).toEqual(true)
+
+    //state狀態不測試 另外外部props也無法測
+    // expect(wrapper.prop('activeIndex1')).toEqual(1)
+    //只確保呼叫外部的props的function
+    expect(wrapper.prop('onTabChange')).toHaveBeenCalledWith(1);
+    // expect(props.onTabChange).toHaveBeenCalledWith(1)
+  })
 })
