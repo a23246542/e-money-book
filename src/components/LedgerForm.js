b078dcb7e95cfb1a69fd6cae44ca7cfaa055b1c6
a23246542 <a23246542@gmail.com>
2020-12-14 18:04:36 +0800

    LedgerFrom創建表單TDD開發

--- src/components/LedgerForm.js
+++ src/components/LedgerForm.js
@@ -1,11 +1,100 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
-const LedgerForm = props => {
+const LedgerForm = ({ledgerItem, onFormSubmit, onCancelSubmit}) => {
+  //畫面表單
+  const [ title, setTitle ] = useState(()=>ledgerItem.title||'');
+  const [ amount, setAmount ] = useState(()=>ledgerItem.price||'');//@但其實是數字
+  const [ date, setDate ] = useState(()=>ledgerItem.date||'');
+  //畫面資料狀態
+  const [ validatePass, setValidatePass ] = useState(false);
+  const [ alertMessage, setAlertMessage ] = useState('');
+
+  const isValidDate = (inputDate) => {
+    const nowTimeStamp = new Date();
+    const selectTimeStamp = Date.parse(inputDate);
+    return selectTimeStamp <= nowTimeStamp;
+    // date.split('-')
+  }
+
+  const submitForm = (e) =>{
+    e.preventDefault();
+    // if(title.trim()==='') {
+    // }
+    console.log('submitForm的值',title,amount,date);
+    if( title && amount && date ) {
+      if(amount<0) {
+        setValidatePass(false);
+        setAlertMessage('數字不能為負');
+      } else if (!isValidDate(date)) {
+        setValidatePass(false);
+        setAlertMessage('不能選擇未來的日期');
+      } else {
+        setAlertMessage('');
+        setValidatePass(true);
+        onFormSubmit();
+        console.log('通過');
+      }
+    } else {
+      setValidatePass(false);
+      setAlertMessage('表格不能為空!');
+      console.log('不通過');
+    }
+  }
+
+  const cancelSubmit = (e) => {
+    e.preventDefault();
+    onCancelSubmit();
+  }
   return (
-    <div>
-      表單
-    </div>
+    //!需要改input閉合 className htmlFor
+    <form>
+      <div className="form-group">
+        <label htmlFor="inputTitle">Email address</label>
+        <input type="text" className="form-control" id="inputTitle" aria-describedby="emailHelp"
+          value={title}
+          onChange={(e)=>{setTitle(e.target.value.trim())}}
+          // ref={(input) => {setTitle(input.current.value)}}//@@原本的操作
+        />
+        <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+      </div>
+      <div className="form-group">
+        <label htmlFor="inputAmount">Password</label>
+        <input type="number" className="form-control" id="inputAmount"
+          value={amount}
+          // onChange={(e)=>{setAmount(e.target.value.trim())}}
+          onChange={(e)=>{setAmount(e.target.value.trim()*1)}}//@@
+          // ref={(input) => {setAmount(input.current.value)}}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="inputDate">Password</label>
+        <input type="date" className="form-control" id="inputDate"
+          value={date}
+          onChange={(e)=>{setDate(e.target.value.trim())}}
+          // ref={(input) => {setDate(input.current.value)}}
+        />
+      </div>
+      {/* <div className="form-group form-check">
+        <input type="checkbox" className="form-check-input" id="inputDate"/>
+        <label className="form-check-label" for="exampleCheck1">Check me out</label>
+      </div> */}
+      { !validatePass&&
+        <div className="alert alert-warning" role="alert">
+          {alertMessage}
+        </div>
+      }
+      <button type="submit"
+        id="submit"
+        className="btn btn-primary"
+        onClick={(e)=>{submitForm(e)}}
+      >
+        Submit
+      </button>
+      <button type="button" id="cancel" className="btn btn-primary"
+        onClick={(e)=>{cancelSubmit(e)}}
+      >Submit</button>
+    </form>
   )
 }
 
--- src/components/__test__/LedgerForm.test.js
+++ src/components/__test__/LedgerForm.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import { testItems } from '../../testData';
 import LedgerForm from '../LedgerForm';
+import { act } from 'react-dom/test-utils';
 
 // 需求分析
 //分創建跟編輯
@@ -10,21 +11,34 @@ import LedgerForm from '../LedgerForm';
 
 //先props
 const props = {
+  ledgerItem: {},
   onFormSubmit: jest.fn(),
   onCancelSubmit: jest.fn()
 }
 
 const props_with_item = {
-
+  ledgerItem: testItems[0],//!
+  onFormSubmit: jest.fn(),
+  onCancelSubmit: jest.fn()
 }
 
 let wrapper,  wrapper2, formInstance;
 
 
+export const getInputValue = (selector, wrapper) => {
+  // return wrapper.instance().find(input).value;//%%% instance沒有find方法!!!
+  // return wrapper.find(selector).instance().value;///%%% ShallowWrapper::instance() can only be called on the root
+  return wrapper.find(selector).instance().value;
+  // return wrapper.getDOMNode().find(selector).instance().value;
+}
+
+export const setInputValue = (selector, newValue, wrapper) => {
+  wrapper.find(selector).instance().value = newValue;
+}
 describe('test LedgerForm component', () => {
   beforeEach(()=>{
-    wrapper = shallow(<LedgerForm {...props}/>);
-    wrapper2 = shallow(<LedgerForm {...props_with_item}/>)
+    wrapper = mount(<LedgerForm {...props}/>);
+    wrapper2 = mount(<LedgerForm {...props_with_item}/>)
     // formInstance =
   })
   it('should render the component to match the snapshot',()=>{//%%
@@ -35,21 +49,82 @@ describe('test LedgerForm component', () => {
   describe('test LedgerForm with no data ', () => {
     //-默認樣式
     //要看到三個input 一個form
-    it('render should see three input and one form element',()=>{})
+    it('render should see three input and one form element',()=>{
+      expect(wrapper.find('input').length).toEqual(3);
+      expect(wrapper.find('form').length).toEqual(1);
+    })
     // 要是空的
-    it('render LedgerForm with no data should see three input and no value',()=>{})
+    it('render LedgerForm with no data should see three input and no value',()=>{
+      expect(getInputValue('#inputTitle',wrapper)).toEqual('');
+      expect(getInputValue('#inputAmount',wrapper).toString()).toEqual('');
+      expect(getInputValue('#inputDate',wrapper)).toEqual('');
+    })
     //-交互
     // 空值提交會出現報錯提示
-    it('submit form with empty input should show alert message',()=>{})
+    it('submit form with empty input should show alert message',()=>{
+      wrapper.find('#submit').simulate('click');
+      // expect(formInstance.state.validatePass).toEqual(false)
+      expect(wrapper.find('.alert').length).toEqual(1);
+      // expect(props.onFormSubmit).toHaveBeenCalled();//%%
+      expect(props.onFormSubmit).not.toHaveBeenCalled();
+    })
     // 價格負的提交會錯
-    it('submit form with invalid price should show alert message',()=>{})
+    it('submit form with invalid price should show alert message',()=>{
+      //%%無法觸發setState更新
+      // setInputValue('#inputTitle', 'xxx', wrapper);
+      // setInputValue('#inputAmount', -20, wrapper);// @@是否另一邊轉字串
+      // setInputValue('#inputDate', '2020-12-14', wrapper);
+      act(()=>{
+        wrapper.find('#inputTitle').simulate('change',{ target:{value:'xxx'}});
+        // wrapper.find('#inputAmount').simulate('change',{ target:{value:-20}});//%%改字串才能trim TypeError: e.target.value.trim is not a function
+        wrapper.find('#inputAmount').simulate('change',{ target:{value:'-40'}});
+        wrapper.find('#inputDate').simulate('change',{ target:{value:'2020-12-14'}});
+      })
+      wrapper.update();
+      wrapper.find('#submit').simulate('click');
+      expect(wrapper.find('.alert').length).toEqual(1);
+      expect(wrapper.find('.alert').text()).toEqual('數字不能為負');
+      expect(props.onFormSubmit).not.toHaveBeenCalled();
+    })
     // 日期不符格式提交會錯
-    it('submit form with invalid date should show alert message',()=>{})
+    it('submit form with invalid date should show alert message',()=>{
+      act(()=>{ //沒加好像也ok
+        wrapper.find('#inputTitle').simulate('change',{ target:{value:'xxx'}});
+        wrapper.find('#inputAmount').simulate('change',{ target:{value:'30'}});
+        wrapper.find('#inputDate').simulate('change',{ target:{value:'2021-12-14'}});
+      })
+      wrapper.update();
+      wrapper.find('#submit').simulate('click');
+      expect(wrapper.find('.alert').length).toEqual(1);
+      expect(wrapper.find('.alert').text()).toEqual('不能選擇未來的日期');
+      expect(props.onFormSubmit).not.toHaveBeenCalled();
+    })
     // 填寫正確提交會觸發回呼帶參數
     // it('submit form with valid data should invoke right function with right item',()=>{})
-    it('submit form with valid data should trigger callback with right object',()=>{})
+    it('submit form with valid data should trigger callback with right object',()=>{
+      act(()=>{ //沒加好像也ok
+        wrapper.find('#inputTitle').simulate('change',{ target:{value:'吃飯'}});
+        wrapper.find('#inputAmount').simulate('change',{ target:{value:'300'}});
+        wrapper.find('#inputDate').simulate('change',{ target:{value:'2020-12-13'}});
+      })
+      wrapper.update();
+      wrapper.find('#submit').simulate('click');
+      // console.log(wrapper.debug());
+      expect(wrapper.find('.alert').length).toEqual(0);
+      expect(props.onFormSubmit).toHaveBeenCalled();
+    })
     // 點取消會觸發回調%%
-    it('click the cancel button should call the right callback',()=>{});
+    it('click the cancel button should call the right callback',()=>{
+      act(()=>{ //沒加好像也ok
+        wrapper.find('#inputTitle').simulate('change',{ target:{value:'吃飯'}});
+        wrapper.find('#inputAmount').simulate('change',{ target:{value:'300'}});
+        wrapper.find('#inputDate').simulate('change',{ target:{value:'2020-12-13'}});
+      })
+      wrapper.update();
+      wrapper.find('#cancel').simulate('click');
+      expect(props.onFormSubmit).not.toHaveBeenCalled();
+      expect(props.onCancelSubmit).toHaveBeenCalled();
+    });
   })
 
   //編輯
