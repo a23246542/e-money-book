f275295ca32db24c9ce16a7835c500a642eca4c2
a23246542 <a23246542@gmail.com>
2021-01-19 14:52:19 +0800

    添加port號

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const root = __dirname + '/build';
+const port = process.env.PORT || 8000;
 server.use(express.static(root, { maxAge: 86400000 }));
 server.use(middlewares);
 const reactRouterWhiteList = ['/create','/edit/:itemId']
@@ -12,6 +13,6 @@ server.get(reactRouterWhiteList, (request, response) =>{
   response.sendFile(path.resolve(root,'index.html'));
 });
 server.use(router);
-server.listen(8000, () => {
+server.listen(port, () => {
   console.log('server is running');
 })
