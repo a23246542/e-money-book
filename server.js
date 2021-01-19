6944047b7abeeba6c437789d96673f84bb415373
a23246542 <a23246542@gmail.com>
2021-01-19 13:45:27 +0800

    優化加入自定義路由

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const jsonServer = require('json-server');
 const express = require('express');
+const path = require('path');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const root = __dirname + '/build';
 server.use(express.static(root, { maxAge: 86400000 }));
 server.use(middlewares);
+const reactRouterWhiteList = ['/create','/edit/:itemId']
+server.get(reactRouterWhiteList, (request, response) =>{
+  response.sendFile(path.resolve(root,'index.html'));
+});
 server.use(router);
 server.listen(8000, () => {
   console.log('server is running');
