ff2378da1023597ac3886ba5280d86dacaa14891
a23246542 <a23246542@gmail.com>
2021-02-02 16:51:48 +0800

    fix修復test env 發api失敗問題

--- src/api.js
+++ src/api.js
@@ -3,11 +3,12 @@ import axios from 'axios';
 axios.defaults.adapter = require('axios/lib/adapters/http');
 
 let api = axios.create({
-  // baseURL: process.env.REACT_APP_URL,
-  baseURL: 'http://localhost:3003',
+  baseURL:
+    process.env.NODE_ENV === 'test'
+      ? 'http://localhost:3003'
+      : process.env.REACT_APP_URL,
   headers: {
     'Content-Type': 'application/json',
-    // 'Content-Type': 'application/x-www-form-urlencoded',
   },
 });
 
