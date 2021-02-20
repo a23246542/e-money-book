95940ecd73a8d29926423fec764d4c882d36fcf7
a23246542 <a23246542@gmail.com>
2021-02-20 18:25:02 +0800

    craco設置webpack alias

--- src/App.js
+++ src/App.js
@@ -6,17 +6,17 @@ import {
   Redirect,
   useRouteMatch,
 } from 'react-router-dom';
-import './App.css';
+import '@/App.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import Home from './containers/Home';
-import Create from './containers/Create';
-import Login from './containers/Login';
+import Home from '@/containers/Home';
+import Create from '@/containers/Create';
+import Login from '@/containers/Login';
 import AppContext, { AppProvider } from './contexts/AppContext';
 import AuthContext from './contexts/AuthContext';
 import { flattenArr, parseToYearsAndMonth, makeID } from './helpers/utility';
 import useFacebookLogin from './hooks/useFacebookLogin';
 import useLedger from './hooks/useLedger';
-import api from './api';
+import api from '@/api';
 
 function App() {
   // const [fbResponse, handleFBLogin, handleFBLogout] = useFacebookLogin({
