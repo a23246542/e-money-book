992b1eddd20330ed0e90acc1cd402f406535cb8b
a23246542 <a23246542@gmail.com>
2021-05-05 16:22:14 +0800

    feat: cross-env添加端口環境參數

--- server.js
+++ server.js
@@ -5,14 +5,15 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const root = __dirname + '/build';
+console.log(process.env.PORT);
 const port = process.env.PORT || 8000;
 server.use(express.static(root, { maxAge: 86400000 }));
 server.use(middlewares);
-const reactRouterWhiteList = ['/create','/edit/:itemId']
-server.get(reactRouterWhiteList, (request, response) =>{
-  response.sendFile(path.resolve(root,'index.html'));
+const reactRouterWhiteList = ['/create', '/edit/:itemId'];
+server.get(reactRouterWhiteList, (request, response) => {
+  response.sendFile(path.resolve(root, 'index.html'));
 });
 server.use(router);
 server.listen(port, () => {
   console.log('server is running');
-})
+});
